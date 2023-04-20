db5e9a063de7793e82654a7b74509301b144c26e
harpreet780 <harpreetsaini276@gmail.com>
2023-04-20 17:17:28 +0530

    select items and show total price

--- src/Components/CarDetails.js
+++ src/Components/CarDetails.js
@@ -1,16 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CarOptions } from "../Pages/Home"
 
 const CarDetails = () => {
+    const [isSelected, setIsSelected] = useState([]);
+    const [showSelectedCard, setShowSelectedCard] = useState(false);
+
     const carInfo = useContext(CarOptions);
 
-    return (
+    const splicedData = carInfo?.carlist?.slice(0, 10);
+    console.debug(carInfo)
 
+    const selectCard = (id) => {
+        splicedData?.filter((carItem) => carItem.id === id).map((i) => {
+            return (
+                setIsSelected([...isSelected, i]),
+                setShowSelectedCard(true)
+            )
+        })
+    }
+
+    return (
         <div>
+            <button className='btn'>Total Price:</button>
+            <span className='mx-2'><b>${carInfo?.showTotalPrice(isSelected)}</b></span>
             <div className='d-flex justify-content-center flex-wrap mt-5'>
-                {carInfo?.carlist?.splice(0, 12).map((item) => {
+                {splicedData?.map((item) => {
                     return (
-                        <div className="card">
+                        <div className={`card ${showSelectedCard === true && "active-card"}`}
+                            onClick={() => selectCard(item.id)}
+                        >
                             <div className="d-flex align-items-center justify-content-between">
                                 <p><b> Id:</b></p>
                                 <p>{item.id}</p>
@@ -28,8 +46,12 @@ const CarDetails = () => {
                                 <p>{item.car_model_year}</p>
                             </div>
                             <div className="d-flex align-items-center justify-content-between">
-                                <p><b>Car color:</b></p>
+                                <p><b>Car Color:</b></p>
                                 <p>{item.car_color}</p>
+                            </div>
+                            <div className="d-flex align-items-center justify-content-between">
+                                <p><b>Car Price:</b></p>
+                                <p>{item.price}</p>
                             </div>
                         </div>
                     )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,27 +5,34 @@ import CarDetails from '../Components/CarDetails';
 const CarOptions = createContext();
 
 const Home = () => {
-    const [carlist, setCarlist]= useState();
+    const [carlist, setCarlist] = useState();
 
-    const CarView = async()=>{
-        return await axios.get('https://myfakeapi.com/api/cars').then((res)=>{
+    const CarView = async () => {
+        return await axios.get('https://myfakeapi.com/api/cars').then((res) => {
             setCarlist(res.data.cars)
-        }).catch((error)=>{
+        }).catch((error) => {
             console.log(error)
         })
     }
+    const showTotalPrice = (totalPrice = []) => {
+        let total = totalPrice.map((item) => {
+            return item.price.replace(/\$/g, '')
+        })
+        let findTotalPrice = total.reduce((a, b) => parseFloat(a) + parseFloat(b), 0)
+        return findTotalPrice.toFixed(2);
+    };
 
-    useEffect(()=>{
+    useEffect(() => {
         CarView()
-    },[])
+    }, [])
 
     return (
         <div className='p-5'>
             <h2 className='text-center text-decoration-underline'>
                 Implement ContextApi
             </h2>
-            <CarOptions.Provider value={{carlist}}>
-               <CarDetails/>
+            <CarOptions.Provider value={{ carlist, showTotalPrice }}>
+                <CarDetails />
             </CarOptions.Provider>
         </div>
     )
